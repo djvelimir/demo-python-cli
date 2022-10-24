9b59da47b9e66880958ebb021546ac01ac76b6c6
Velimir Đurković <velimir.djurkovic@gmail.com>
2022-10-24 20:50:34 +0200

    Added password generator logic

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,43 @@
+import random
 from password_generator_base import PasswordGeneratorBase
 
 
 class PasswordGenerator(PasswordGeneratorBase):
+    __PASSWORD_LENGTH: int = 16
+    __UPPERCASE_CHARACTERS: str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    __LOWERCASE_CHARACTERS: str = "abcdefghijklmnopqrstuvwxyz"
+    __DIGIT_CHARACTERS: str = "0123456789"
+    __SPECIAL_CHARACTERS: str = "~`!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?"
+    __UNION_OF_ALLOWED_CHARACTERS: str =\
+        __UPPERCASE_CHARACTERS +\
+        __LOWERCASE_CHARACTERS +\
+        __DIGIT_CHARACTERS +\
+        __SPECIAL_CHARACTERS
+
     def generate(self) -> str:
-        return 'jk@zfLQzW7Ug&8mb'
+        password: str = ''
+
+        # generate at least one uppercase character
+        password = password + self.__generate_random_character(self.__UPPERCASE_CHARACTERS)
+
+        # generate at least one lowercase character
+        password = password + self.__generate_random_character(self.__LOWERCASE_CHARACTERS)
+
+        # generate at least one digit character
+        password = password + self.__generate_random_character(self.__DIGIT_CHARACTERS)
+
+        # generate at least one special character
+        password = password + self.__generate_random_character(self.__SPECIAL_CHARACTERS)
+
+        for i in range(4, self.__PASSWORD_LENGTH):
+            # generate random character from union of allowed characters
+            password = password + self.__generate_random_character(self.__UNION_OF_ALLOWED_CHARACTERS)
+
+        # shuffle generated characters
+        password = ''.join(random.sample(password, len(password)))
+
+        return password
+
+    @staticmethod
+    def __generate_random_character(characters: str) -> str:
+        return characters[random.randint(0, len(characters) - 1)]
